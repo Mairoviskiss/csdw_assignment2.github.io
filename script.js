eb1e5bcf2685c3ecba651aeb95af94b01d8ab618
Maíra Araújo <maira.araujo@student.griffith.ie>
2021-05-13 18:18:01 +0100

    Fixing nutella slides - WE ARE THE BEST

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -5,22 +6,31 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+
 function showSlides(n) {
+  
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides2");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
+   if (n > slides.length) {
       slideIndex = 1;
+     // document.getElementsByClassName("mySlides").style.display = 'none';
     }
   if (n < 1) {
       slideIndex = slides.length;
+    //  document.getElementsByClassName("mySlides").style.display = 'none';
     }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    // document.getElementsByClassName("mySlides").style.display = 'none';
+    slides[i].style.display = "none";
+      
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+   //document.getElementsByClassName("mySlides").style.display = 'none'; 
+        dots[i].className = dots[i].className.replace(" active", ""); 
+          
   }
+ //document.getElementsByClassName("mySlides").style.display = 'none';
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
+}*/
